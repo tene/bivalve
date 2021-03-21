214d7483542d1962e4ceb5b8489c4154bcaf456b
Stephen Weeks <tene@allalone.org>
2021-03-21 00:11:10 -0700

    rename

--- shell-engine/src/main.rs
+++ shell-engine/src/main.rs
@@ -1,7 +1,7 @@
 use std::path::PathBuf;
 use structopt::StructOpt;
 
-use bivalve::{
+use shell_engine::{
     exec::Execute,
     rt::{simple::Simple, Runtime},
 };
@@ -15,7 +15,7 @@ struct Opt {
 fn main() {
     let opt = Opt::from_args();
     let mut rt = Simple::new();
-    if let Ok(mut parser) = bivalve::parse::parse_file(opt.file) {
+    if let Ok(mut parser) = shell_engine::parse::parse_file(opt.file) {
         while let Ok(Some(cmd)) = parser.complete_command() {
             cmd.exec(&mut rt);
         }
